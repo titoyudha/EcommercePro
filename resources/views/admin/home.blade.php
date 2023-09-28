5f21cf3b8ddcde252b4b11600c9bc38530070fe2
titoyudha <tito.yudha.bimashika@gmail.com>
2023-09-28 23:32:43 +0700

    create admin views

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,19 +1,21 @@
-<x-app-layout>
-
-</x-app-layout>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    @include('admin.css')
 </head>
 
 <body>
-    <h3>Admin</h3>
+    <div class="container-scroller">
+        <!-- partial:partials/_sidebar.html -->
+        @include('admin.sidebar')
+        <!-- partial -->
+        @include('admin.header')
+        <!-- partial -->
+        @include('admin.body')
+        <!-- container-scroller -->
+        @include('admin.script')
+
 </body>
 
 </html>
